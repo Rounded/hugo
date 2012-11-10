900f7ff09f511e664ab4f01095ea45940efd5c8d
Brian Weinreich <be.weinreich@gmail.com>
2012-11-10 16:42:31 -0500

    rails environment added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  ActionMailer::Base.mail(:from => "be.weinreich@gmail.com", :to => "be.weinreich@gmail.com", :subject => "#{DateTime.now}", :body => "#{DateTime.now}").deliver
+  ActionMailer::Base.mail(:from => "be.weinreich@gmail.com", :to => "be.weinreich@gmail.com", :subject => "#{DateTime.now}", :body => "#{DateTime.now} #{Rails.env}").deliver
 
 end
