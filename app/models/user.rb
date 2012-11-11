6920a61606932cb6a4452954987968b1247b8fb9
Eric Candino <ec@roundedco.com>
2012-11-10 21:36:38 -0500

    signup working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User
   include Mongoid::Document
+  include Mongoid::Timestamps
   field :name
   field :number, type: Integer
 
@@ -7,22 +8,23 @@ class User
   validates_presence_of :number
   validates_numericality_of :number
 
+  before_create :remove_dashes
   after_create :send_first_text
+
 
   protected
 
-  # def remove_dashes
-  #   number = number.gsub(/\D/, '')
-  # end
+  def remove_dashes
+    number = self.number.to_s.gsub(/[^0-9]/, "")
+    self.number = number.to_i
+  end
 
   def send_first_text
     Telapi.config do |config|
       config.account_sid = 'ACbbcf2b79541543ff86dd9955952d1076'
       config.auth_token  = 'bc039f3d301a4475899cb9e31cb3d953'
     end
-    alerted_places.each do |venue|
-     Telapi::Message.create( '(716) 341-5129', '(302) 752-4624', "#{venue.name}\n#{venue.location['address']}\n#{venue.location.city}, #{venue.location.state} #{venue.location['postalCode']}\n#{venue.contact['formattedPhone']}" )
-    end
+    Telapi::Message.create( "#{number}", '(302) 752-4624', "Thanks for signing up #{name}." )
   end
 
 end
